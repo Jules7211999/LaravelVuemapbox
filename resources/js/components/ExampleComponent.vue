7c623c27b2bae3d2752e68d4f42f20eda4f821d6
Jules Leomel <julesparcon1999@gmail.com>
2021-05-06 11:06:17 +0800

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="mapContainer"></div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+
+export default {
+  name: "BaseMap",
+  data() {
+    return {
+      accessToken: "pk.eyJ1IjoianVsZXNsZW9tZWwiLCJhIjoiY2ttYnJhZTU4MDNucjJ2bGozd3lhMjI2ciJ9.pJW1MGbeoeYe-jbXaA1XgQ",
+    };
+  },
+  mounted() {
+    mapboxgl.accessToken = this.accessToken;
+
+    new mapboxgl.Map({
+      container: "mapContainer",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [103.811279, 1.345399],
+      zoom: 12,
+      maxBounds: [
+        [103.6, 1.1704753],
+        [104.1, 1.4754753],
+      ],
+    });
+  },
+};
+</script>
+
+<style>
+#mapContainer{
+ width: 100vh;
+ height: 1000px;
+ background-color: black;
+}
+</style>
+
